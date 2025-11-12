b05a0f44b72380065279b3529559b4e0093bb4d8
nethasa-manawadu <dunithi.nethasa@gmail.com>
2025-11-12 13:00:53 +0530

    created the main hero | tools

--- cmn/app/products/tools/page.tsx
+++ cmn/app/products/tools/page.tsx
@@ -1,9 +1,11 @@
 import ProductSection from "@/components/ProductSection";
 import HeroCarousel from "@/components/HeroCarousel";
+import ToolsHeroSection from "@/components/ToolsHeroSection";
 
 export default function Home() {
   return (
     <main>
+      <ToolsHeroSection />
       <HeroCarousel />
       <ProductSection />
     </main>
