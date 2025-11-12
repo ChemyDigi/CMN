90848369efad44434348d73fbcd68969fcb2837b
nethasa-manawadu <dunithi.nethasa@gmail.com>
2025-11-12 12:47:43 +0530

    created the hero carousel

--- cmn/app/products/tools/page.tsx
+++ cmn/app/products/tools/page.tsx
@@ -1,8 +1,10 @@
 import ProductSection from "@/components/ProductSection";
+import HeroCarousel from "@/components/HeroCarousel";
 
 export default function Home() {
   return (
     <main>
+      <HeroCarousel />
       <ProductSection />
     </main>
   );
