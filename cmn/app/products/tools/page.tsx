6c05df83ace26dea530634315c311bfff9308896
AshenFerna29 <ashen2742@gmail.com>
2025-11-14 17:17:11 +0530

    Components name changed

--- cmn/app/products/tools/page.tsx
+++ cmn/app/products/tools/page.tsx
@@ -1,15 +1,19 @@
 import ProductSection from "@/components/Tools/ProductSection";
 import HeroCarousel from "@/components/Tools/GeneralHeroCarousel";
 import ToolsHeroSection from "@/components/Tools/ToolsHeroSection";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <ToolsHeroSection />
       <HeroCarousel 
         page="products" 
       />
       <ProductSection />
+      <Footer />
     </main>
   );
 }
