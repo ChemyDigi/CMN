8a46e7f2b9969a710beac4da53d6ff6039991dcd
AshenFerna29 <ashen2742@gmail.com>
2025-11-16 14:11:08 +0530

    Found some issues and fixed them all

--- cmn/app/products/ref-ac/page.tsx
+++ cmn/app/products/ref-ac/page.tsx
@@ -2,13 +2,17 @@ import HeroCarousel from "@/components/GeneralHeroCarousel";
  
 import RefAcHeroSection from "@/components/AC-Ref/refrigeratorHero";
 import ProductSection from "@/components/AC-Ref/ProductSection";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer"; 
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <RefAcHeroSection />
       <HeroCarousel page="RefAC" />
      <ProductSection/>
+     <Footer />
     </main>
   );
 }
--- cmn/next.config.ts
+++ cmn/next.config.ts
@@ -2,11 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "media.istockphoto.com", // for iStock images
-      "images.unsplash.com",   // optional: for Unsplash images
-      "cdn.pixabay.com",       // optional: for Pixabay images
-      "firebasestorage.googleapis.com", // if you later use Firebase Storage URLs
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.istockphoto.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com"
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com"
+      },
+      {
+        protocol: "https",
+        hostname: "lfirebasestorage.googleapis.com"
+      }
     ],
   },
 };
