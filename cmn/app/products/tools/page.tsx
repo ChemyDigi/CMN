76dd303c03bc52a8110c8d230a74caf0ee4ec997
Naduni Peiris <peirisnaduni13@gmail.com>
2025-11-21 11:12:20 +0530

    Intro text added to both products

--- cmn/app/products/tools/page.tsx
+++ cmn/app/products/tools/page.tsx
@@ -2,15 +2,13 @@ import ProductSection from "@/components/Tools/ProductSection";
 import ToolsHeroSection from "@/components/Tools/ToolsHeroSection";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
+import IntroText from "@/components/Tools/IntroText";
 export default function Home() {
   return (
     <main>
       <Navbar />
       <ToolsHeroSection />
-      {/* <HeroCarousel 
-        page="products" 
-      /> */}
+      <IntroText/>
       <ProductSection />
       <Footer />
     </main>
---
+++ cmn/components/AC-Ref/IntroText.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function IntroText() {
+  return (
+    <section className="relative w-full bg-white">
+      {/* Hero Text Section */}
+      <div className="container mx-auto px-4 py-10 text-center sm:px-6 md:px-8 lg:py-12 xl:px-12">
+        <h1 className="mb-3 text-xl font-semibold tracking-tight text-black 
+                        sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl">
+          Engineered for Your Comfort
+        </h1>
+
+        <p className="mx-auto text-xs text-black sm:max-w-md sm:text-sm 
+                      md:max-w-xl md:text-base lg:max-w-2xl lg:text-lg">
+          From powerful climate control to advanced food preservation, our appliances
+          are built for reliability, efficiency, and the modern home.
+        </p>
+      </div>
+    </section>
+  );
+}
